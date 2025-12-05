72faa0cd0fc21700c4a645f479f6c178a9c27f4d
Dnyaneshwar-Markad <pavanmarkad810.com>
2025-12-05 16:20:19 +0530

    05-12-2025 updating the connection string

--- router.py
+++ router.py
@@ -6,19 +6,19 @@ def go_to(page):
     return page
 
 def get_connection():
-    # #For NeonDB
-    # return psycopg.connect(
-    #     "postgresql://neondb_owner:npg_rs1bVogh7EtU@ep-weathered-math-a1pj9ocn-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-    # )
-    
-    ## For Local Postgres DB
+    #For NeonDB
     return psycopg.connect(
-        host="localhost",
-        dbname="Maharashta",      # your db name
-        user="postgres",             # your postgres username
-        password="sa@123",        # your postgres password
-        port=5432
+        "postgresql://neondb_owner:npg_rs1bVogh7EtU@ep-weathered-math-a1pj9ocn-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     )
+    
+    # ## For Local Postgres DB
+    # return psycopg.connect(
+    #     host="localhost",
+    #     dbname="Maharashta",      # your db name
+    #     user="postgres",             # your postgres username
+    #     password="sa@123",        # your postgres password
+    #     port=5432
+    # )
 
     # return pyodbc.connect(
     #     "DRIVER={ODBC Driver 17 for SQL Server};"

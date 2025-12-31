b41947606090961f321083108a88100a86c4d01e
Dnyaneshwar-Markad <pavanmarkad810.com>
2025-12-31 10:59:38 +0530

    ordered the part no in DESC for  voters-data/count api

--- login_api.py
+++ login_api.py
@@ -1482,7 +1482,7 @@ def get_voters_data_counts(
                     FROM "VoterList"
                     WHERE {where_clause}
                     GROUP BY "PartNo"
-                    ORDER BY "PartNo"
+                    ORDER BY COUNT(*) DESC
                     ''',
                     params
                 )
